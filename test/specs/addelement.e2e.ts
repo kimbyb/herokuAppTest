0e10d995c2fda410100e53bfb9c89fba290c5592
Sasha <s.marchuk95@gmail.com>
2023-07-08 23:50:18 +0200

    prettier

--- test/specs/addelement.e2e.ts
+++ test/specs/addelement.e2e.ts
@@ -3,28 +3,20 @@ import SecurePage from '../pageobjects/secure.page.js'
 
 describe('Adding elements', () => {
     let elementAdded: boolean;
-
     it('should click add element', async () => {
         await AddElementPage.open();
-
         await AddElementPage.clickAdd();
-
         await expect(AddElementPage.deleteButton).toBeExisting();
-
         elementAdded = true;
     });
 
     it('should delete elements', async () => {
-
         if (!elementAdded) {
             // If no element was added in the previous test, you can skip this test or perform additional actions.
             return;
         }
-
         await AddElementPage.clickDelete();
-
         await expect(AddElementPage.deleteButton).not.toBeExisting();
         await browser.pause(5000)
-
     });
 });
